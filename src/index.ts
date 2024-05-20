83e1eddf8cd2a3637d75c46e174820f4fdcd83fb
Serg032 <sergioradigales@gmail.com>
2024-05-20 23:48:57 +0200

    matches router added

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import "dotenv/config";
 import clubsRouter from "./app/clubs-router";
 import courtsRouter from "./app/courts-router";
 import membersRouter from "./app/members-router";
+import matchesRouter from "./app/matches-router";
 
 const app = express();
 const port = 3000;
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use("/clubs", clubsRouter);
 app.use("/courts", courtsRouter);
 app.use("/members", membersRouter);
+app.use('/matches', matchesRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
